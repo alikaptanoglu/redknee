f7b7d8f9450243a7d99c705518026a633ab01f1b
wutlu <wutlu@yandex.ru>
2016-12-09 00:18:10 +0300

    social network link

--- jsON/drawer.php
+++ jsON/drawer.php
@@ -39,6 +39,7 @@ ob_start();
 				<a class="list-group-item disabled" href="pages-app-landing.html">App Landing <span class="badge">Coming Soon</span></a>
 				<a class="list-group-item<?php get('page', 'resume', ' active');?>" href="pages-resume.html">Resume</a>
 				<a class="list-group-item<?php get('page', 'sign', ' active');?>" href="pages-sign.html">Sign</a>
+				<a class="list-group-item disabled" href="pages-social.html">Social Network <span class="badge">Coming Soon</span></a>
 				<a class="list-group-item disabled" href="pages-capital-film.html">Capital Film <span class="badge">Coming Soon</span></a>
 				<a class="list-group-item disabled" href="pages-fashion.html">Fashion <span class="badge">Coming Soon</span></a>
 				<a class="list-group-item disabled" href="pages-fitness.html">Fitness <span class="badge">Coming Soon</span></a>
