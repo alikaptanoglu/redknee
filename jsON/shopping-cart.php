0284b7c51d824c739032e3e0e857af8a9223a99b
wutlu <wutlu@yandex.ru>
2016-12-16 17:29:13 +0300

    shopping

--- jsON/shopping-cart.php
+++ jsON/shopping-cart.php
@@ -4,7 +4,24 @@ header('Content-Type => application/json');
 ob_start();
 ?>
 <div class="container-fluid">
-asdasd
+	<div class="pdt-10 pdb-10">
+		<ul class="list-group">
+			<li class="list-group-item">
+				<div class="float">
+					<div class="col col-xs-6">
+						<img class="img-responsive img-rounded" src="upload/day-washingmachine.png" alt="..." />
+					</div>
+					<div class="col col-xs-6">
+						<label>Total</label>
+						<input type="number" value="1" name="total" class="form-control" />
+					</div>
+				</div>
+				<p>
+					<a href="#">Samsung WW8PJ3282KW/AH A+++ 8KG 1200RPM Washing</a>
+				</p>
+			</li>
+		</ul>
+	</div>
 </div>
 <?php
 $content = ob_get_clean();
